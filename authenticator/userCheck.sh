e1eb5b6aeace0cba8b7f6ef320ba6560a6754364
drl3427 <drl3427@rit.edu>
2023-10-24 18:37:49 -0400

    Changed userCheck to run every 3 mins and have the script run indefinitely

--- authenticator/userCheck.sh
+++ authenticator/userCheck.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-output=$(who -a | grep +)
-epochtime=$(date +%s)
-filename="sessions-$epochtime"
-#echo "$filename"
-ssh user@ip echo $output > ./$filename
+while true; do
+    output=$(who -a | grep +)
+    epochtime=$(date +%s)
+    filename="sessions-$epochtime"
+    #echo "$filename"
+    ssh user@ip echo $output > ./$filename
+done
