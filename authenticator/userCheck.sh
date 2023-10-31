8661e8506dd1deef8e9fd9e23457ca94b74ccfa6
Joe MacDonough <jtmacdonough@gmail.com>
2023-10-31 13:35:31 -0400

    power controller auth script

--- authenticator/userCheck.sh
+++ authenticator/userCheck.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-output=$(who -a | grep +)
-epochtime=$(date +%s)
-filename="sessions-$epochtime"
-#echo "$filename"
-ssh user@ip echo $output > ./$filename
+while :
+do
+        output=$(who -a | grep +)
+        epochtime=$(date +%s)
+        filename="sessions-$epochtime"
+        echo "$filename"
+        #echo $output | ssh auth@100.64.2.85 cat > './logs/${filename}'
+        echo $output | ssh auth@100.64.2.85 "cat > /home/auth/logs/${filename}"
+        ssh auth@100.64.2.85 pwd
+        sleep 30
+done    
+
